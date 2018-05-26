94ba8d03f7cbd7cd6319246a38026af3f9bd72de
user <avichai.kalij@gmail.com>
2018-05-26 18:52:55 +0300

    add files

---
+++ notes.js
@@ -0,0 +1,23 @@
+console.log('starting notes.js');
+
+var addNote = (title,body)=>{
+
+
+console.log('adding ' + title);
+};
+
+var removeNote = (title)=>{
+    
+console.log('removing ' + title);
+};
+
+var getAll = ()=>{
+    
+    console.log('get all' );
+    };
+
+module.exports = {
+    addNote,
+    removeNote
+};
+
